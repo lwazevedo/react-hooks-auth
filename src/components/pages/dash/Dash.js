bce65001e5187b1f4c30b0c2bcc94d8e247f3415
pssofluiz <luiz.azevedo@topaziopartner.com.br>
2020-03-16 14:41:32 -0300

    finish dash

--- src/components/pages/dash/Dash.js
+++ src/components/pages/dash/Dash.js
@@ -1,7 +1,63 @@
 import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { Grid } from '@material-ui/core';
+import { Budget, TotalUsers, TasksProgress, TotalProfit } from './components';
 
-const Dash = () => (
-  <p>Dash</p>
-);
+const useStyles = makeStyles(theme => ({
+  root: {
+    padding: theme.spacing(4)
+  }
+}));
+
+
+const Dash = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid
+        container
+        spacing={4}
+      >
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <Budget />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <TotalUsers />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <TasksProgress />
+        </Grid>
+        <Grid
+          item
+          lg={3}
+          sm={6}
+          xl={3}
+          xs={12}
+        >
+          <TotalProfit />
+        </Grid>
+
+      </Grid>
+    </div>
+  )
+};
 
 export default Dash;
---
+++ src/components/pages/dash/components/index.js
@@ -0,0 +1,5 @@
+import Budget from './Budget/Budget'
+import TotalUsers from './TotalUsers/TotalUsers'
+import TasksProgress from './TasksProgress/TasksProgress'
+import TotalProfit from './TotalProfit/TotalProfit'
+export { Budget, TotalUsers, TasksProgress, TotalProfit };
